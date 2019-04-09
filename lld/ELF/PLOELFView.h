4d5905e97462665b7c9ae8664e94258be4d95bd0
Han Shen <shenhan@google.com>
2019-04-09 14:29:22 -0700

    WIP to use ELFObjectFile.h

--- lld/ELF/PLOELFView.h
+++ lld/ELF/PLOELFView.h
@@ -5,6 +5,7 @@
 #include <map>
 #include <memory>
 
+#include "lld/Common/ErrorHandler.h"
 #include "llvm/ADT/iterator_range.h"
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/BinaryFormat/ELF.h"
@@ -14,6 +15,7 @@
 
 using llvm::ArrayRef;
 using llvm::MemoryBufferRef;
+using llvm::object::ELFFile;
 using llvm::StringRef;
 
 using std::list;
@@ -35,55 +37,22 @@ class ELFBlock {
   };
 
   ELFBlock(Ty TP, const StringRef &SR)
-      : Type(TP), Data(SR), Writable(nullptr) {}
-
-  ELFBlock(Ty TP, uint32_t BlockSize)
-      : Type(TP), Data(),
-        Writable(new char[BlockSize]), WritableSize(BlockSize) {}
-
+      : Type(TP), Data(SR) {}
   ~ELFBlock() {}
 
   Ty getType() const { return Type; }
 
-  char *getWritableData() {
-    assert(Writable.get());
-    return Writable.get();
-  }
-
   const char* getContent() const {
-    if (isWritable()) return Writable.get();
     return Data.data();
   }
 
   uint64_t getSize() const {
-    return isWritable() ? WritableSize : Data.size();
-  }
-
-  void resizeOnWrite(uint64_t NewSize) {
-    if (getSize() == NewSize) return;
-    assert (getSize() < NewSize);
-    char *NewBuf = new char[NewSize];
-    memcpy(NewBuf, getContent(), getSize());
-    Writable.reset(NewBuf);
-    WritableSize = NewSize;
-  }
-
-  void copyOnWrite() {
-    if (isWritable()) return;
-    Writable.reset(new char[Data.size()]);
-    memcpy(Writable.get(), Data.data(), Data.size());
-    WritableSize = Data.size();
-  }
-
-  bool isWritable() const {
-    return Writable.get() != nullptr;
+    return Data.size();
   }
 
  protected:
   Ty        Type;
   StringRef Data;
-  std::unique_ptr<char> Writable;
-  uint64_t              WritableSize;
 };
 
 class ELFView {
@@ -102,29 +71,17 @@ class ELFView {
 
   virtual bool Init() = 0;
 
-  virtual void AppendSection(StringRef Name,
-                             uint32_t Type,
-                             uint64_t Flags,
-                             uint64_t Addr,
-                             uint64_t &OutputOffset,
-                             uint64_t Size,
-                             uint32_t Link,
-                             uint32_t Info,
-                             uint64_t Align,
-                             uint64_t EntSize,
-                             char *Content) = 0;
-
-  virtual void Diagnose() const = 0;
-  virtual bool Write(const int OutFd) const = 0;
-  virtual void SortShdrs() = 0;
-
   virtual void BuildCfgs() = 0;
 
   StringRef       ViewName;
   const uint32_t  Ordinal;
   MemoryBufferRef FileRef;
+
+  map<StringRef, unique_ptr<ELFCfg>> Cfgs;
+  void EraseCfg(ELFCfg *&CfgPtr);
+
+  // Minimal data maintained for reading into ELF file.
   std::list<std::unique_ptr<ELFBlock>> Blocks;
-  // These iterators are properly maintained before and after any modification.
   BlockIter EhdrPos;           // ELF header block.
   BlockIter FirstSectPos;      // First sect.
   BlockIter FirstShdrPos;      // First section header.
@@ -135,16 +92,15 @@ class ELFView {
   BlockIter SymTabStrSectPos;  // Symbol table string table section.
   BlockIter SymTabStrShdrPos;  // Symbol table string table section header.
 
-  map<StringRef, unique_ptr<ELFCfg>> Cfgs;
-  void EraseCfg(ELFCfg *&CfgPtr);
 };
 
 template <class ELFT>
 class ELFViewImpl : public ELFView {
- public:
+public:
   // ELFTUInt is uint64_t when ELFT::Is64 is true, otherwise uint32_t.
   using ELFTUInt     = typename ELFT::uint;
-  using ViewFile     = llvm::object::ELFFile<ELFT>;
+
+  using ViewFile     = ELFFile<ELFT>;
   using ViewFileEhdr = typename ELFT::Ehdr;
   using ViewFileShdr = typename ELFT::Shdr;
   using ViewFileRela = typename ELFT::Rela;
@@ -152,45 +108,13 @@ class ELFViewImpl : public ELFView {
 
   ELFViewImpl(const StringRef &VN,
 	      const uint32_t Ordinal,
-	      const MemoryBufferRef &FR) : ELFView(VN, Ordinal, FR) {}
+	      const MemoryBufferRef &FR)
+    : ELFView(VN, Ordinal, FR) {}
   virtual ~ELFViewImpl() {}
 
   bool Init() override;
 
-  void Diagnose() const override;
-
-  bool Write(const int OutFd) const override;
-
-  void AppendSection(StringRef SecName,
-                     uint32_t  SecType,
-                     uint64_t  SecFlags,
-                     uint64_t  SecAddr,
-                     uint64_t &OutputSecOffset,
-                     uint64_t  SecSize,
-                     uint32_t  SecLink,
-                     uint32_t  SecInfo,
-                     uint64_t  SecAlign,
-                     uint64_t  SecEntSize,
-                     char     *SecContent) override;
-
-  void SortShdrs() override;
-
   void BuildCfgs() override;
-
-  bool check() const;
-
-  const ViewFileEhdr *getEhdr(const ELFBlock *VB) const {
-    assert(VB->getType() == ELFBlock::Ty::EHDR_BLK);
-    if (VB->getType() != ELFBlock::Ty::EHDR_BLK) return nullptr;
-    return static_cast<const ViewFileEhdr *>((const void *)(VB->getContent()));
-  }
-
-  ViewFileEhdr *getWritableEhdr(ELFBlock *VB) {
-    assert(VB->getType() == ELFBlock::Ty::EHDR_BLK);
-    VB->copyOnWrite();
-    if (VB->getType() != ELFBlock::Ty::EHDR_BLK) return nullptr;
-    return static_cast<ViewFileEhdr *>((void *)(VB->getWritableData()));
-  }
 
   const ViewFileShdr *getShdr(const ELFBlock *VB) const {
     assert(VB->getType() == ELFBlock::Ty::SHDR_BLK);
@@ -202,27 +126,6 @@ class ELFViewImpl : public ELFView {
     BlockIter P = FirstShdrPos;
     for (int I = 0; I != shidx; ++I, ++P) {}
     return getShdr(P->get());
-  }
-
-  ViewFileShdr *getWritableShdr(ELFBlock *VB) {
-    assert(VB->getType() == ELFBlock::Ty::SHDR_BLK);
-    VB->copyOnWrite();
-    if (VB->getType() != ELFBlock::Ty::SHDR_BLK) return nullptr;
-    return static_cast<ViewFileShdr *>((void *)(VB->getWritableData()));
-  }
-
-  const ELFBlock *getSect(const uint16_t SecIdx) const {
-    auto A = FirstSectPos;
-    for (uint16_t I = 0; I != SecIdx; ++I, ++A) {}
-    return A->get();
-  }
-
-  llvm::iterator_range<BlockIter> getSectBlocks() {
-    return llvm::iterator_range<BlockIter>(FirstSectPos, FirstShdrPos);
-  }
-
-  llvm::iterator_range<BlockIter> getShdrBlocks() {
-    return llvm::iterator_range<BlockIter>(FirstShdrPos, Blocks.end());
   }
 
   // Get Relas for section "shndx".
@@ -263,39 +166,8 @@ class ELFViewImpl : public ELFView {
   }
 
   uint64_t getSectionSize(uint16_t Shndx) const {
-    return uint16_t(getShdr(Shndx)->sh_size);
-  }
-
-  ELFBlock *createShdrBlock(uint32_t SecName,
-                            uint32_t SecType,
-                            ELFTUInt SecFlags,
-                            ELFTUInt SecAddr,
-                            ELFTUInt SecOffset,
-                            ELFTUInt SecSize,
-                            uint32_t SecLink,
-                            uint32_t SecInfo,
-                            ELFTUInt SecAlign,
-                            ELFTUInt SecEntSize) {
-    ELFBlock *ShdrBlk = new ELFBlock(
-        ELFBlock::Ty::SHDR_BLK, sizeof(ViewFileShdr));
-    ViewFileShdr *Shdr = getWritableShdr(ShdrBlk);
-    Shdr->sh_name = SecName;
-    Shdr->sh_type = SecType;
-    Shdr->sh_flags = SecFlags;
-    Shdr->sh_addr = SecAddr;
-    Shdr->sh_offset = SecOffset;
-    Shdr->sh_size = SecSize;
-    Shdr->sh_link = SecLink;
-    Shdr->sh_info = SecInfo;
-    Shdr->sh_addralign = SecAlign;
-    Shdr->sh_entsize = SecEntSize;
-    return ShdrBlk;
-  }
-
-  const ELFBlock *getSecBlock(uint16_t shidx) const {
-    BlockIter P = FirstSectPos;
-    for (int I = 0; I != shidx; ++I, ++P) {}
-    return P->get();
+    return ELFT::Is64Bits ? uint64_t(getShdr(Shndx)->sh_size) :
+      uint32_t(getShdr(Shndx)->sh_size);
   }
 
   bool setupSymTabAndSymTabStrPos() {
@@ -320,13 +192,6 @@ class ELFViewImpl : public ELFView {
  private:
   bool initEhdr(const ViewFile &VF);
   bool initSections(const ViewFile &VF);
-  ELFTUInt alignTo(ELFTUInt F) {
-    uint32_t A = sizeof(ELFTUInt);  // for ELF64LE, A = 8.
-    if ((F & (A - 1)) != 0)
-      F = (F & (~(A - 1))) + A;
-    return F;
-  }
-
 };
 
 }  // namespace plo
