0d2a7b40b9e108cbda9774fd5ea54c5ec406ed51
Rahman Lavaee <rahmanl@google.com>
2019-09-12 09:49:06 -0700

    Remove plo/paths.mk

--- plo/paths.mk
+++
@@ -1,5 +0,0 @@
-RELEASE_LLVM_BIN=/g/tmsriram/Projects_2019/github_repo/release_build/install/bin
-BOLT=/usr/local/google/Projects_2018/FaceBookBOLT/build_release_bolt/bin/llvm-bolt
-PERF2BOLT=/usr/local/google/Projects_2018/FaceBookBOLT/build_release_bolt/bin/perf2bolt
-CREATE_LLVM_PROF_DIR=/g/tmsriram/Projects_2019/create_llvm_prof_dir
-CREATE_LLVM_PROF=/g/tmsriram/Projects_2019/create_llvm_prof_dir/create_llvm_prof
