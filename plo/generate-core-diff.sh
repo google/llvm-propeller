5e182d7e09b84eb9735c187925cca6ba287e5a74
Han Shen <shenhan@google.com>
2019-11-11 10:29:29 -0800

    Minor fix to patch generating script.

--- plo/generate-core-diff.sh
+++ plo/generate-core-diff.sh
@@ -12,9 +12,12 @@ if ! git log "${MERGE_BASE}" 1>/dev/null 2>&1 ; then
     exit 1
 fi
 
+echo "Generating diff against \"$(git log --oneline -1 ${MERGE_BASE})\" ..."
+
 DDIR="$(cd $(dirname $0) && pwd)"
 PATCH_FILE=${DDIR}"/plo-dev-diff-on-$(echo ${MERGE_BASE} | cut -c-8)"
+pushd ${DDIR}/../ 1>/dev/null 2>&1
 git diff ${MERGE_BASE} -- llvm/ clang/ compiler-rt/ lld/ ./create_llvm_prof > ${PATCH_FILE}
+popd 1>/dev/null 2>&1
 
 echo "Generated patch file: $PATCH_FILE"
-
