aaf928dc6c94a81bdb934c75a73522140c8be2fe
Rahman Lavaee <rahmanl@google.com>
2020-01-14 23:49:13 -0800

    Refactoring WIP

--- lld/ELF/Propeller/PropellerBBReordering.cpp
+++ lld/ELF/Propeller/PropellerBBReordering.cpp
@@ -98,81 +98,6 @@ using llvm::detail::DenseMapPair;
 
 namespace lld {
 namespace propeller {
-const unsigned ClusterMergeSizeThreshold = 1 << 22;
-
-std::string toString(MergeOrder mOrder){
-  switch (mOrder) {
-    case MergeOrder::X2X1Y:
-      return "X2X1Y";
-    case MergeOrder::X1YX2:
-      return "X1YX2";
-    case MergeOrder::X2YX1:
-      return "X2YX1";
-    case MergeOrder::YX2X1:
-      return "YX2X1";
-    default:
-      assert("Invalid MergeOrder!" && false);
-      return "";
-  }
-}
-
-std::string toString(const NodeChain& c){
-  std::string str;
-  size_t cfgNameLength = c.DelegateNode->CFG->Name.size();
-  str += c.DelegateNode->CFG->Name.str() + " [ ";
-  for(auto* n: c.Nodes){
-    str += n->CFG->getEntryNode()==n ? "Entry" : std::to_string(n->ShName.size() - cfgNameLength - 4);
-    str += " (size=" + std::to_string(n->ShSize) + ", freq=" + std::to_string(n->Freq) + ")";
-    if (n!=c.Nodes.back())
-      str += " -> ";
-  }
-  str += " ]";
-  str += " score: " + std::to_string(c.Score);
-  return str;
-}
-
-std::string NodeChainBuilder::toString(NodeChainAssembly &assembly) const{
-  std::string str("assembly record between:\n");
-  str += lld::propeller::toString(*assembly.splitChain()) + " as X\n";
-  str += lld::propeller::toString(*assembly.unsplitChain()) + " as Y\n";
-  //str += "split position (X):, " + std::to_string(assembly.SlicePosition - assembly.splitChain()->Nodes.begin()) + "\n";
-  str += "merge order: " + lld::propeller::toString(assembly.MOrder) + "\n";
-  str += "ScoreGain: " + std::to_string(assembly.ScoreGain);
-  return str;
-}
-
-// Return the Extended TSP score for one edge, given its source to sink
-// direction and distance in the layout.
-inline double getEdgeExtTSPScore(const CFGEdge &edge, bool isEdgeForward,
-                          uint64_t srcSinkDistance) {
-  if (edge.isCall()){
-    if (isEdgeForward)
-      srcSinkDistance += edge.Src->ShSize / 2;
-    else
-      srcSinkDistance -= edge.Src->ShSize / 2;
-  }
-
-  if (edge.isReturn()){
-    if (isEdgeForward)
-      srcSinkDistance += edge.Sink->ShSize / 2;
-    else
-      srcSinkDistance -= edge.Sink->ShSize / 2;
-  }
-
-  if (srcSinkDistance == 0 && (edge.Type == CFGEdge::EdgeType::INTRA_FUNC || edge.Type == CFGEdge::EdgeType::INTRA_DYNA))
-    return edge.Weight * propellerConfig.optFallthroughWeight;
-
-  if (isEdgeForward && srcSinkDistance < propellerConfig.optForwardJumpDistance)
-    return edge.Weight * propellerConfig.optForwardJumpWeight *
-           (1.0 -
-            ((double)srcSinkDistance) / propellerConfig.optForwardJumpDistance);
-
-  if (!isEdgeForward && srcSinkDistance < propellerConfig.optBackwardJumpDistance)
-    return edge.Weight * propellerConfig.optBackwardJumpWeight *
-           (1.0 -
-            ((double)srcSinkDistance) / propellerConfig.optBackwardJumpDistance);
-  return 0;
-}
 
 void NodeChainBuilder::init() {
   for (ControlFlowGraph* cfg: CFGs){
@@ -251,7 +176,7 @@ void NodeChainBuilder::coalesceChains() {
 void NodeChainBuilder::mergeChains(NodeChain *leftChain,
                                    NodeChain *rightChain) {
   if(leftChain->Freq==0 ^ rightChain->Freq==0)
-    error("Attempting to merge hot and cold chains: \n" + lld::propeller::toString(*leftChain) + "\nAND\n" + lld::propeller::toString(*rightChain));
+    error("Attempting to merge hot and cold chains: \n" + toString(*leftChain) + "\nAND\n" + toString(*rightChain));
 
   mergeInOutEdges(leftChain, rightChain);
 
@@ -843,64 +768,6 @@ void NodeChainBuilder::mergeAllChains() {
   }
 }
 
-// This function computes the ExtTSP score for a chain assembly record. This
-// goes the three BB slices in the assembly record and considers all edges
-// whose source and sink belongs to the chains in the assembly record.
-double NodeChainAssembly::computeExtTSPScore() const {
-  double score = 0;
-
-  auto visit = [this, &score] (CFGEdge& edge, NodeChain * srcChain, NodeChain *sinkChain){
-        uint8_t srcSliceIdx, sinkSliceIdx;
-        auto srcNodeOffset = edge.Src->ChainOffset;
-        auto sinkNodeOffset = edge.Sink->ChainOffset;
-        if (!findSliceIndex(edge.Src, srcChain, srcNodeOffset, srcSliceIdx))
-          return;
-
-        if (!findSliceIndex(edge.Sink, sinkChain, sinkNodeOffset, sinkSliceIdx))
-          return;
-
-        bool edgeForward = (srcSliceIdx < sinkSliceIdx) ||
-                           (srcSliceIdx == sinkSliceIdx &&
-                            (srcNodeOffset + edge.Src->ShSize <= sinkNodeOffset));
-
-        uint64_t srcSinkDistance = 0;
-
-        if (srcSliceIdx == sinkSliceIdx) {
-          srcSinkDistance = edgeForward
-                       ? sinkNodeOffset - srcNodeOffset - edge.Src->ShSize
-                       : srcNodeOffset - sinkNodeOffset + edge.Src->ShSize;
-        } else {
-            const NodeChainSlice &srcSlice = Slices[srcSliceIdx];
-            const NodeChainSlice &sinkSlice = Slices[sinkSliceIdx];
-            srcSinkDistance = edgeForward
-                         ? srcSlice.EndOffset - srcNodeOffset - edge.Src->ShSize +
-                               sinkNodeOffset - sinkSlice.BeginOffset
-                         : srcNodeOffset - srcSlice.BeginOffset +
-                               edge.Src->ShSize + sinkSlice.EndOffset -
-                               sinkNodeOffset;
-          // Increment the distance by the size of the middle slice if the src
-          // and sink are from the two ends.
-          if (std::abs(sinkSliceIdx - srcSliceIdx) == 2)
-            srcSinkDistance += Slices[1].size();
-        }
-
-        score += getEdgeExtTSPScore(edge, edgeForward, srcSinkDistance);
-      };
-
-
-  score += unsplitChain()->Score;
-
-  if (split())
-    splitChain()->forEachOutEdgeToChain(splitChain(), visit);
-  else
-    score += splitChain()->Score;
-
-  splitChain()->forEachOutEdgeToChain(unsplitChain(), visit);
-  unsplitChain()->forEachOutEdgeToChain(splitChain(), visit);
-
-  return score;
-}
-
 void NodeChainBuilder::doOrder(std::unique_ptr<ChainClustering> &CC){
   fprintf(stderr, "ORDERING FOR: %s\n", CFGs.back()->Name.str().c_str());
   init();
@@ -929,179 +796,6 @@ void NodeChainBuilder::doOrder(std::unique_ptr<ChainClustering> &CC){
 
   for(auto& elem: Chains)
     CC->addChain(std::move(elem.second));
-}
-
-void ChainClustering::addChain(std::unique_ptr<NodeChain>&& chain_ptr){
-  for(CFGNode *n: chain_ptr->Nodes)
-    n->Chain = chain_ptr.get();
-  auto& chainList = ((propellerConfig.optReorderIP || propellerConfig.optSplitFuncs || propellerConfig.optReorderFuncs) && chain_ptr->Freq==0) ? ColdChains : HotChains;
-  chainList.push_back(std::move(chain_ptr));
-}
-
-// Initialize a cluster containing a single chain an associates it with a unique
-// id.
-ChainClustering::Cluster::Cluster(NodeChain *chain)
-    : Chains(1, chain), DelegateChain(chain) {}
-
-// Returns the most frequent caller of a function. This function also gets as
-// the second parameter the cluster containing this function to save a lookup
-// into the ChainToClusterMap.
-ChainClustering::Cluster *
-CallChainClustering::getMostLikelyPredecessor(NodeChain * chain,
-                                              Cluster *cluster) {
-  DenseMap<Cluster*, uint64_t> clusterEdge;
-
-  for(CFGNode * n: chain->Nodes){
-    if (!propellerConfig.optReorderIP && !n->isEntryNode())
-      continue;
-    auto visit = [&clusterEdge, n, chain, cluster, this] (CFGEdge& edge){
-      if (!edge.Weight)
-        return;
-      if (edge.isReturn())
-        return;
-      auto *caller = edge.Src->Chain;
-      if (!caller)
-        return;
-      auto * callerCluster = ChainToClusterMap[caller];
-      assert(caller->Freq);
-      if (caller == chain || callerCluster == cluster)
-        return;
-      if (callerCluster->Size > ClusterMergeSizeThreshold)
-        return;
-      // Ignore calls which are cold relative to the callee
-      if (edge.Weight * 10 < n->Freq)
-        return;
-      // Do not merge if the caller cluster's density would degrade by more than
-      // 1/8.
-      if (8 * callerCluster->Size * (cluster->Weight * callerCluster->Weight) <
-          callerCluster->Weight * (cluster->Size + callerCluster->Size))
-        return;
-      clusterEdge[callerCluster] += edge.Weight;
-    };
-    n->forEachInEdgeRef(visit);
-
-  }
-
-  auto bestCaller = std::max_element(clusterEdge.begin(), clusterEdge.end(), [] (const DenseMapPair<Cluster*, uint64_t>& p1,
-                                                               const DenseMapPair<Cluster*, uint64_t>& p2) {
-    if (p1.second == p2.second)
-      return std::less<Cluster*>()(p1.first, p2.first);
-    return p1.second < p2.second;
-  });
-
-  if (bestCaller == clusterEdge.end())
-    return nullptr;
-  return bestCaller->first;
-}
-
-
-void ChainClustering::sortClusters(std::vector<Cluster *> &clusterOrder) {
-  for (auto &p : Clusters)
-    clusterOrder.push_back(p.second.get());
-
-  auto clusterComparator = [](Cluster *c1, Cluster *c2) -> bool {
-    // Set a deterministic order when execution densities are equal.
-    if (c1->getDensity() == c2->getDensity())
-      return c1->DelegateChain->DelegateNode->MappedAddr <
-          c2->DelegateChain->DelegateNode->MappedAddr;
-    return c1->getDensity() > c2->getDensity();
-  };
-
-  std::sort(clusterOrder.begin(), clusterOrder.end(), clusterComparator);
-}
-
-void NoOrdering::doOrder(std::vector<CFGNode*> &hotOrder,
-                                  std::vector<CFGNode*> &coldOrder){
-  auto chainComparator = [](const std::unique_ptr<NodeChain> &c_ptr1,
-                            const std::unique_ptr<NodeChain> &c_ptr2) -> bool {
-    return c_ptr1->DelegateNode->MappedAddr < c_ptr2->DelegateNode->MappedAddr;
-  };
-
-  std::sort(HotChains.begin(), HotChains.end(), chainComparator);
-  std::sort(ColdChains.begin(), ColdChains.end(), chainComparator);
-
-  for(auto& c_ptr: HotChains)
-    for(CFGNode* n: c_ptr->Nodes)
-      hotOrder.push_back(n);
-
-  for(auto& c_ptr: ColdChains)
-    for(CFGNode* n: c_ptr->Nodes)
-    coldOrder.push_back(n);
-}
-
-// Merge clusters together based on the CallChainClustering algorithm.
-void CallChainClustering::mergeClusters() {
-  // Build a map for the execution density of each chain.
-  DenseMap<NodeChain *, double> chainWeightMap;
-
-  for(auto& c_ptr: HotChains){
-    NodeChain * chain = c_ptr.get();
-    chainWeightMap.try_emplace(chain, chain->execDensity());
-  }
-
-  // Sort the hot chains in decreasing order of their execution density.
-  std::sort(HotChains.begin(), HotChains.end(),
-            [&chainWeightMap] (const std::unique_ptr<NodeChain> &c_ptr1,
-                               const std::unique_ptr<NodeChain> &c_ptr2){
-              auto chain1Weight = chainWeightMap[c_ptr1.get()];
-              auto chain2Weight = chainWeightMap[c_ptr2.get()];
-              if (chain1Weight == chain2Weight)
-                return c_ptr1->DelegateNode->MappedAddr < c_ptr2->DelegateNode->MappedAddr;
-              return chain1Weight > chain2Weight;
-            });
-
-  for (auto& c_ptr : HotChains){
-    NodeChain* chain = c_ptr.get();
-    if (chainWeightMap[chain] <= 0.005)
-      break;
-    auto *cluster = ChainToClusterMap[chain];
-    // Ignore merging if the cluster containing this function is bigger than
-    // 2MBs (size of a large page).
-    if (cluster->Size > ClusterMergeSizeThreshold)
-      continue;
-    assert(cluster);
-
-    Cluster *predecessorCluster = getMostLikelyPredecessor(chain, cluster);
-    if (!predecessorCluster)
-      continue;
-
-    //assert(predecessorCluster != cluster && predecessorChain != chain);
-    mergeTwoClusters(predecessorCluster, cluster);
-  }
-}
-
-
-void ChainClustering::doOrder(std::vector<CFGNode*> &hotOrder,
-                                  std::vector<CFGNode*> &coldOrder){
-  //warn("[propeller]" + Twine(HotChains.size())+ " Hot chains and " + Twine(ColdChains.size()) + " Cold chains.");
-  initClusters();
-  mergeClusters();
-  std::vector<Cluster *> clusterOrder;
-  DenseMap<ControlFlowGraph *, size_t> ChainOrder;
-  sortClusters(clusterOrder);
-  for (Cluster *cl: clusterOrder)
-    for(NodeChain* c: cl->Chains)
-      for(CFGNode *n: c->Nodes) {
-        ChainOrder.try_emplace(n->CFG, hotOrder.size());
-        hotOrder.push_back(n);
-      }
-
-  auto coldChainComparator = [&ChainOrder](const std::unique_ptr<NodeChain> &c_ptr1,
-                                           const std::unique_ptr<NodeChain> &c_ptr2) -> bool {
-    if (c_ptr1->CFG && c_ptr2->CFG) {
-      if (c_ptr1->CFG->isHot() != c_ptr2->CFG->isHot())
-        return c_ptr1->CFG->isHot();
-      if (c_ptr1->CFG->isHot() && c_ptr2->CFG->isHot() && (c_ptr1->CFG != c_ptr2->CFG))
-        return ChainOrder[c_ptr1->CFG] < ChainOrder[c_ptr2->CFG];
-    }
-    return c_ptr1->DelegateNode->MappedAddr < c_ptr2->DelegateNode->MappedAddr;
-  };
-
-  std::sort(ColdChains.begin(), ColdChains.end(), coldChainComparator);
-
-  for(auto &c_ptr: ColdChains)
-    for(CFGNode* n: c_ptr->Nodes)
-    coldOrder.push_back(n);
 }
 
 void PropellerBBReordering::printStats() {
@@ -1156,20 +850,6 @@ void PropellerBBReordering::printStats() {
   fprintf(stderr, "\n");
 }
 
-
-bool NodeChainAssembly::CompareNodeChainAssembly::operator () (
-    const std::unique_ptr<NodeChainAssembly> &a1,
-    const std::unique_ptr<NodeChainAssembly> &a2) const {
-
-  if (a1->ScoreGain == a2->ScoreGain){
-    if (std::less<std::pair<NodeChain*,NodeChain*>>()(a1->ChainPair,a2->ChainPair))
-        return true;
-    if (std::less<std::pair<NodeChain*,NodeChain*>>()(a2->ChainPair,a1->ChainPair))
-        return false;
-    return a1->assemblyStrategy() < a2->assemblyStrategy();
-  }
-  return a1->ScoreGain < a2->ScoreGain;
-}
 
 } // namespace propeller
 } // namespace lld
---
+++ lld/ELF/Propeller/PropellerNodeChain.cpp
@@ -0,0 +1,26 @@
+#include "PropellerNodeChain.h"
+
+namespace lld {
+namespace propeller {
+
+std::string toString(const NodeChain &c) {
+  std::string str;
+  size_t cfgNameLength = c.DelegateNode->CFG->Name.size();
+  str += c.DelegateNode->CFG->Name.str() + " [ ";
+  for (auto *n : c.Nodes) {
+    str += n->CFG->getEntryNode() == n
+               ? "Entry"
+               : std::to_string(n->ShName.size() - cfgNameLength - 4);
+    str += " (size=" + std::to_string(n->ShSize) +
+           ", freq=" + std::to_string(n->Freq) + ")";
+    if (n != c.Nodes.back())
+      str += " -> ";
+  }
+  str += " ]";
+  str += " score: " + std::to_string(c.Score);
+  return str;
+}
+
+} //namespace propeller
+} //namespace lld
+
