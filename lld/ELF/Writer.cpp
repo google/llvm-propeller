b1c22ff2e0702a04b2e0e69e1acfa0fc109fc511
Sriraman Tallam <tmsriram@google.com>
2020-04-06 23:39:42 -0700

    Address D68065 comments.

--- lld/ELF/Writer.cpp
+++ lld/ELF/Writer.cpp
@@ -1794,8 +1794,7 @@ template <class ELFT> void Writer<ELFT>::optimizeBasicBlockJumps() {
     // consecutive jump instructions can be flipped so that a fall
     // through jmp instruction can be deleted.
     parallelForEachN(0, sections.size(), [&](size_t i) {
-      InputSection *next =
-          (i + 1) < sections.size() ? sections[i + 1] : nullptr;
+      InputSection *next = i + 1 < sections.size() ? sections[i + 1] : nullptr;
       InputSection &is = *sections[i];
       if (!noneOptimizableSections.count(&is))
         result[i] =
@@ -2201,7 +2200,8 @@ template <class ELFT> void Writer<ELFT>::finalizeSections() {
   finalizeSynthetic(in.ppc64LongBranchTarget);
 
   // Relaxation to delete inter-basic block jumps created by basic block
-  // sections. Run after in.symTab is finalized.
+  // sections. Run after in.symTab is finalized as optimizeBasicBlockJumps
+  // relaxes jump instructions based on symbol offset.
   if (config->optimizeBBJumps)
     optimizeBasicBlockJumps();
 
