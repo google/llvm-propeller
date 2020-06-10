7dd2c57e04107d027283c8ee7b31e7781d21354d
Sriraman Tallam <tmsriram@google.com>
2020-06-10 00:26:40 -0700

    Refactor code according to D78851 reviewer comments.

--- llvm/lib/CodeGen/AsmPrinter/DwarfCompileUnit.cpp
+++ llvm/lib/CodeGen/AsmPrinter/DwarfCompileUnit.cpp
@@ -589,14 +589,11 @@ void DwarfCompileUnit::attachRangesOrLowHighPC(
     const auto *BeginMBB = R.first->getParent();
     const auto *EndMBB = R.second->getParent();
 
-    if (BeginMBB->sameSection(EndMBB)) {
-      // If begin and end share their section, there is just one continuous
-      // range.
-      List.push_back({BeginLabel, EndLabel});
-      continue;
-    }
-
     const auto *MBB = BeginMBB;
+    // Basic block sections allows basic block subsets to be placed in unique
+    // sections.  For each section, the begin and end label must be added to the
+    // list.  If there is more than one range, debug ranges must be used.
+    // Otherwise, High PC can be used.
     do {
       if (MBB->sameSection(EndMBB) || MBB->isEndSection()) {
         auto MBBSectionRange = Asm->MBBSectionRanges[MBB->getSectionID()];
