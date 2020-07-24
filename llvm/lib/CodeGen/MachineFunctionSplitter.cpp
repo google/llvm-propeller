488ad24e976a995351d73e4917127cec5f07577c
Snehasish Kumar <snehasishk@google.com>
2020-07-24 14:34:30 -0700

    Fix minor type in description.

--- llvm/lib/CodeGen/MachineFunctionSplitter.cpp
+++ llvm/lib/CodeGen/MachineFunctionSplitter.cpp
@@ -1,4 +1,4 @@
-//===-- FEntryInsertion.cpp - Patchable prologues for LLVM -------------===//
+//===-- MachineFunctionSplitter.cpp - Patchable prologues for LLVM -------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
