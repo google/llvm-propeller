b1cd66824518c74257ed8358dfa6baf1287242d9
Daniel Hoekwater <hoekwater@google.com>
2024-07-26 11:01:38 -0700

    No public description
    PiperOrigin-RevId: 656461105

---
+++ propeller/branch_aggregation_test.cc
@@ -0,0 +1,29 @@
+#include "propeller/branch_aggregation.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace propeller {
+namespace {
+
+using ::testing::UnorderedElementsAre;
+
+TEST(BranchAggregation, GetNumberOfBranchCounters) {
+  EXPECT_EQ((BranchAggregation{.branch_counters = {{{.from = 1, .to = 2}, 3},
+                                                   {{.from = 3, .to = 4}, 5}}}
+                 .GetNumberOfBranchCounters()),
+            8);
+}
+
+TEST(BranchAggregation, GetUniqueAddresses) {
+  EXPECT_THAT(
+      (BranchAggregation{.branch_counters = {{{.from = 1, .to = 2}, 1},
+                                             {{.from = 3, .to = 3}, 1}},
+                         .fallthrough_counters = {{{.from = 3, .to = 3}, 1},
+                                                  {{.from = 4, .to = 5}, 1}}}
+           .GetUniqueAddresses()),
+      UnorderedElementsAre(1, 2, 3, 4, 5));
+}
+
+}  // namespace
+}  // namespace propeller
