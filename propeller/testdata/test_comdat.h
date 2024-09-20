2b0d2fd19337deab1fdd68e0eed80d44f8b67173
Daniel Hoekwater <hoekwater@google.com>
2024-09-20 12:53:29 -0700

    Add a genrule for a binary with comdat functions
    PiperOrigin-RevId: 676940573

---
+++ propeller/testdata/test_comdat.h
@@ -0,0 +1,9 @@
+#ifndef PROPELLER_TESTDATA_TEST_COMDAT_H_
+#define PROPELLER_TESTDATA_TEST_COMDAT_H_
+
+class Foo {
+ public:
+  int do_work() { return 13; }
+};
+
+#endif  // PROPELLER_TESTDATA_TEST_COMDAT_H_
