f348fae29d24e9b250824c2eb55437d043c3cd7d
Daniel Hoekwater <hoekwater@google.com>
2024-10-09 14:14:19 -0700

    Add macros for absl::Status
    PiperOrigin-RevId: 684168210

---
+++ propeller/status_macros.h
@@ -0,0 +1,98 @@
+//
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#ifndef PROPELLER_STATUS_MACROS_H_
+#define PROPELLER_STATUS_MACROS_H_
+
+#include "absl/log/log.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+
+// A set of helpers to manipulate absl::Status and absl::StatusOr.
+
+namespace propeller_testing {
+namespace internal_status {
+
+inline const absl::Status& GetStatus(const absl::Status& status) {
+  return status;
+}
+
+template <typename T>
+inline const absl::Status& GetStatus(const absl::StatusOr<T> status_or) {
+  return status_or.status();
+}
+
+template <typename T>
+inline const absl::Status& GetStatus(
+    const absl::StatusOr<std::unique_ptr<T>>& status_or) {
+  return status_or.status();
+}
+}  // namespace internal_status
+}  // namespace propeller_testing
+
+#ifndef CONCAT_IMPL
+#define CONCAT_IMPL(x, y) x##y
+#endif
+
+#ifndef CONCAT_MACRO
+#define CONCAT_MACRO(x, y) CONCAT_IMPL(x, y)
+#endif
+
+#define ASSIGN_OR_RETURN(lhs, rexpr) \
+  ASSIGN_OR_RETURN_IMPL(CONCAT_MACRO(_status_or, __COUNTER__), lhs, rexpr)
+
+#define ASSIGN_OR_RETURN_IMPL(statusor, lhs, rexpr) \
+  auto statusor = (rexpr);                          \
+  if (!statusor.ok()) {                             \
+    return statusor.status();                       \
+  }                                                 \
+  lhs = std::move(statusor.value())
+
+#define RETURN_IF_ERROR(expr)                                                \
+  do {                                                                       \
+    const ::absl::Status _status = (expr);                                   \
+    if (!_status.ok()) {                                                     \
+      LOG(ERROR) << "Return Error: " << #expr << " failed with " << _status; \
+      return _status;                                                        \
+    }                                                                        \
+  } while (0)
+
+#define RET_CHECK_OK(status)                                                   \
+  do {                                                                         \
+    const ::absl::Status _status =                                             \
+        ::propeller_testing::internal_status::GetStatus(status);               \
+    if (!_status.ok()) {                                                       \
+      LOG(ERROR) << "Return Error: " << #status << " failed with " << _status; \
+      return absl::InternalError("RET_CHECK_OK fails");                        \
+    }                                                                          \
+  } while (0)
+
+#define RET_CHECK_EQ(lhs, rhs)                                              \
+  do {                                                                      \
+    if ((lhs) != (rhs)) {                                                   \
+      LOG(ERROR) << "RET_CHECK_EQ fails: lhs=" << #lhs << ", rhs=" << #rhs; \
+      return absl::InternalError("RET_CHECK_EQ fails");                     \
+    }                                                                       \
+  } while (0)
+
+#define RET_CHECK_LT(lhs, rhs)                                              \
+  do {                                                                      \
+    if (!((lhs) < (rhs))) {                                                 \
+      LOG(ERROR) << "RET_CHECK_LT fails: lhs=" << #lhs << ", rhs=" << #rhs; \
+      return absl::InternalError("RET_CHECK_LT fails");                     \
+    }                                                                       \
+  } while (0)
+
+#endif  // PROPELLER_STATUS_MACROS_H_
---
+++ propeller/status_macros_test.cc
@@ -0,0 +1,85 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// see the license for the specific language governing permissions and
+// limitations under the license.
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/status/status_matchers.h"
+#include "propeller/status_macros.h"
+#include "propeller/status_macros.h"
+
+namespace {
+
+using ::absl_testing::IsOk;
+using ::absl_testing::IsOkAndHolds;
+using ::absl_testing::StatusIs;
+using ::testing::Test;
+
+TEST(StatusMatchersTest, AssignOrReturn) {
+  auto assigns_or_returns =
+      [](const absl::StatusOr<int> &status_or) -> absl::StatusOr<int> {
+    ASSIGN_OR_RETURN(int x, status_or);
+    return x;
+  };
+
+  EXPECT_THAT(assigns_or_returns(absl::StatusOr<int>(1)), IsOkAndHolds(1));
+  EXPECT_THAT(assigns_or_returns(absl::AbortedError("aborted")),
+              StatusIs(absl::StatusCode::kAborted));
+}
+
+TEST(StatusMatchersTest, ReturnIfError) {
+  auto returns_if_error =
+      [](const absl::Status &status) -> absl::StatusOr<int> {
+    RETURN_IF_ERROR(status);
+    return 1;
+  };
+
+  EXPECT_THAT(returns_if_error(absl::OkStatus()), IsOkAndHolds(1));
+  EXPECT_THAT(returns_if_error(absl::InternalError("internal error")),
+              StatusIs(absl::StatusCode::kInternal));
+}
+
+TEST(StatusMatchersTest, RetCheckOk) {
+  auto ret_check_ok = [](const absl::Status &status) -> absl::Status {
+    RET_CHECK_OK(status);
+    return absl::OkStatus();
+  };
+
+  EXPECT_THAT(ret_check_ok(absl::OkStatus()), IsOk());
+  EXPECT_THAT(ret_check_ok(absl::AbortedError("aborted")),
+              StatusIs(absl::StatusCode::kInternal));
+}
+
+TEST(StatusMatchersTest, RetCheckEq) {
+  auto ret_check_eq = [](int lhs, int rhs) -> absl::Status {
+    RET_CHECK_EQ(lhs, rhs);
+    return absl::OkStatus();
+  };
+
+  EXPECT_THAT(ret_check_eq(1, 1), IsOk());
+  EXPECT_THAT(ret_check_eq(1, 2), StatusIs(absl::StatusCode::kInternal));
+}
+
+TEST(StatusMatchersTest, RetCheckLt) {
+  auto ret_check_lt = [](int lhs, int rhs) -> absl::Status {
+    RET_CHECK_LT(lhs, rhs);
+    return absl::OkStatus();
+  };
+
+  EXPECT_THAT(ret_check_lt(1, 2), IsOk());
+  EXPECT_THAT(ret_check_lt(2, 1), StatusIs(absl::StatusCode::kInternal));
+}
+
+}  // namespace
