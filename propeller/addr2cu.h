1c3f2ccbb7c3d0493791d82ace736ea0aa02e970
Daniel Hoekwater <hoekwater@google.com>
2024-10-16 08:03:50 -0700

    Add a library for translating address to compile unit
    PiperOrigin-RevId: 686509481

---
+++ propeller/addr2cu.h
@@ -0,0 +1,42 @@
+#ifndef PROPELLER_ADDR2CU_H_
+#define PROPELLER_ADDR2CU_H_
+
+#include <cstdint>
+#include <memory>
+
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+#include "llvm/DebugInfo/DWARF/DWARFContext.h"
+#include "llvm/DebugInfo/DWARF/DWARFFormValue.h"
+#include "llvm/Object/ObjectFile.h"
+
+namespace propeller {
+
+// Creates an `llvm::DWARFContext` instance, which can then be used to create
+// an `Addr2Cu` instance.
+absl::StatusOr<std::unique_ptr<llvm::DWARFContext>> CreateDWARFContext(
+    const llvm::object::ObjectFile &obj, absl::string_view dwp_file = "");
+
+// Utility class that gets the module name for a code address with
+// the help of debug information.
+class Addr2Cu {
+ public:
+  explicit Addr2Cu(llvm::DWARFContext &dwarf_context)
+      : dwarf_context_(dwarf_context) {}
+
+  Addr2Cu(const Addr2Cu&) = delete;
+  Addr2Cu& operator=(const Addr2Cu&) = delete;
+
+  Addr2Cu(Addr2Cu &&) = delete;
+  Addr2Cu& operator=(Addr2Cu &&) = delete;
+
+  // Returns the file name for the compile unit that contains the given code
+  // address. Note: the returned string_view lives as long as `dwarf_context_`.
+  absl::StatusOr<absl::string_view> GetCompileUnitFileNameForCodeAddress(
+      uint64_t code_address);
+
+ private:
+  llvm::DWARFContext &dwarf_context_;
+};
+}  // namespace propeller
+#endif  // PROPELLER_ADDR2CU_H_
---
+++ propeller/addr2cu_test.cc
@@ -0,0 +1,131 @@
+#include "propeller/addr2cu.h"
+
+#include <cstdint>
+#include <fstream>
+#include <ios>
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "propeller/status_testing_macros.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "absl/log/check.h"
+#include "absl/status/status.h"
+#include "absl/status/status_matchers.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_format.h"
+#include "absl/strings/string_view.h"
+#include "llvm/DebugInfo/DWARF/DWARFContext.h"
+#include "llvm/Object/ObjectFile.h"
+#include "llvm/Support/Error.h"
+#include "llvm/Support/ErrorOr.h"
+#include "llvm/Support/MemoryBuffer.h"
+
+namespace {
+
+using ::absl_testing::IsOkAndHolds;
+using ::absl_testing::StatusIs;
+using ::propeller::Addr2Cu;
+using ::propeller::CreateDWARFContext;
+using ::testing::HasSubstr;
+
+uint64_t GetSymbolAddress(const std::string &symmap, absl::string_view symbol) {
+  std::ifstream fin(symmap.c_str());
+  int64_t addr;
+  std::string sym_type;
+  std::string sym_name;
+  while (fin >> std::dec >> addr >> sym_type >> sym_name)
+    if (sym_name == symbol) return addr;
+  return -1;
+}
+
+struct BinaryData {
+  std::unique_ptr<llvm::MemoryBuffer> mem_buf;
+  std::unique_ptr<llvm::object::ObjectFile> object_file;
+};
+
+// Primes `BinaryData` for test cases.
+absl::StatusOr<BinaryData> SetupBinaryData(absl::string_view binary) {
+  llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> mem_buf =
+      llvm::MemoryBuffer::getFile(binary);
+  if (!mem_buf) {
+    return absl::FailedPreconditionError(absl::StrCat(
+        "failed creating MemoryBuffer: %s", mem_buf.getError().message()));
+  }
+
+  llvm::Expected<std::unique_ptr<llvm::object::ObjectFile>> object_file =
+      llvm::object::ObjectFile::createELFObjectFile(**mem_buf);
+  if (!object_file) {
+    return absl::FailedPreconditionError(
+        absl::StrFormat("failed creating ELFObjectFile: %s",
+                        llvm::toString(object_file.takeError())));
+  }
+
+  return BinaryData{.mem_buf = std::move(*mem_buf),
+                    .object_file = std::move(*object_file)};
+}
+
+TEST(Addr2CuTest, ComdatFunc) {
+  const std::string binary =
+      absl::StrCat(::testing::SrcDir(),
+                   "_main/propeller/testdata/"
+                   "test_comdat.bin");
+  const std::string symmap =
+      absl::StrCat(::testing::SrcDir(),
+                   "_main/propeller/testdata/"
+                   "test_comdat.symmap");
+
+  ASSERT_OK_AND_ASSIGN(BinaryData binary_data, SetupBinaryData(binary));
+
+  ASSERT_OK_AND_ASSIGN(
+      std::unique_ptr<llvm::DWARFContext> context,
+      CreateDWARFContext(*binary_data.object_file));
+
+  EXPECT_THAT(
+      Addr2Cu(*context).GetCompileUnitFileNameForCodeAddress(
+          GetSymbolAddress(symmap, /*symbol=*/"_ZN3Foo7do_workEv")),
+      IsOkAndHolds("propeller/testdata/test_comdat_1.cc"));
+}
+
+TEST(Addr2CuTest, ComdatFuncHasNoDwp) {
+  const std::string binary =
+      absl::StrCat(::testing::SrcDir(),
+                   "_main/propeller/testdata/"
+                   "test_comdat_with_dwp.bin");
+
+  ASSERT_OK_AND_ASSIGN(BinaryData binary_data, SetupBinaryData(binary));
+
+  EXPECT_THAT(CreateDWARFContext(*binary_data.object_file),
+              StatusIs(absl::StatusCode::kFailedPrecondition,
+                       HasSubstr("without a corresponding dwp file")));
+}
+
+TEST(Addr2CuTest, ComdatFuncHasDwp) {
+  const std::string binary =
+      absl::StrCat(::testing::SrcDir(),
+                   "_main/propeller/testdata/"
+                   "test_comdat_with_dwp.bin");
+  const std::string symmap =
+      absl::StrCat(::testing::SrcDir(),
+                   "_main/propeller/testdata/"
+                   "test_comdat_with_dwp.symmap");
+  const std::string dwp =
+      absl::StrCat(::testing::SrcDir(),
+                   "_main/propeller/testdata/"
+                   "test_comdat_with_dwp.dwp");
+
+  ASSERT_OK_AND_ASSIGN(BinaryData binary_data, SetupBinaryData(binary));
+
+  ASSERT_OK_AND_ASSIGN(
+      std::unique_ptr<llvm::DWARFContext> context,
+      CreateDWARFContext(*binary_data.object_file, dwp));
+
+  EXPECT_THAT(
+      Addr2Cu(*context)
+          .GetCompileUnitFileNameForCodeAddress(
+              GetSymbolAddress(symmap, "_ZN3Foo7do_workEv")),
+      IsOkAndHolds("propeller/testdata/test_comdat_1.cc"));
+}
+}  // namespace
