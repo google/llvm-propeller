c32c7091d8e80feb64e762e02dca24810d7a3128
Daniel Hoekwater <hoekwater@google.com>
2024-11-07 12:35:04 -0800

    Add a data structure for dynamic CFG nodes
    PiperOrigin-RevId: 694212288

---
+++ propeller/cfg_node.cc
@@ -0,0 +1,71 @@
+#include "propeller/cfg_node.h"
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+#include "absl/strings/str_cat.h"
+#include "propeller/cfg_edge_kind.h"
+
+namespace propeller {
+
+std::string CFGNode::GetName() const {
+  std::string bb_name = absl::StrCat(function_index());
+  if (!is_entry()) absl::StrAppend(&bb_name, ".", bb_index(), ".id", bb_id());
+  if (clone_number() != 0) absl::StrAppend(&bb_name, ".c", clone_number());
+  return bb_name;
+}
+
+CFGEdge *CFGNode::GetEdgeTo(const CFGNode &node,
+                            propeller::CFGEdgeKind kind) const {
+  for (const std::vector<CFGEdge *> *edges : {&intra_outs_, &inter_outs_}) {
+    for (CFGEdge *edge : *edges) {
+      if (edge->kind() != kind) continue;
+      if (edge->sink() == &node) return edge;
+    }
+  }
+  return nullptr;
+}
+int CFGNode::CalculateFrequency() const {
+  // A node (basic block) may have multiple outgoing calls to different
+  // functions. In that case, a single execution of that node counts toward
+  // the weight of each of its calls as wells as returns back to the
+  // callsites. To avoid double counting, we only consider the heaviest
+  // call-out and return-in towards calculating the node's frequency. This
+  // mitigates the case discussed in b/155488527 at the expense of possible
+  // underestimation. The underestimation may happen when these calls and
+  // returns occur in separate LBR stacks. Another source of underestimation
+  // is indirect calls. A node may only have one indirect call instruction,
+  // but if different functions are called by that indirect call, the node's
+  // frequency is equal to the aggregation of call-outs rather than their max.
+
+  int max_call_out = 0;
+  int max_ret_in = 0;
+
+  // Total incoming edge frequency to the node's entry (first instruction).
+  int sum_in = 0;
+  // Total outgoing edge frequency from the node's exit (last instruction).
+  int sum_out = 0;
+
+  for (auto *out_edges : {&inter_outs_, &intra_outs_}) {
+    for (auto &edge : *out_edges) {
+      if (edge->IsCall()) {
+        max_call_out = std::max(max_call_out, edge->weight());
+      } else {
+        sum_out += edge->weight();
+      }
+    }
+  }
+
+  for (auto *in_edges : {&inter_ins_, &intra_ins_}) {
+    for (auto &edge : *in_edges) {
+      if (edge->IsReturn()) {
+        max_ret_in = std::max(max_ret_in, edge->weight());
+      } else {
+        sum_in += edge->weight();
+      }
+    }
+  }
+  return std::max({max_call_out, max_ret_in, sum_out, sum_in});
+}
+}  // namespace propeller
---
+++ propeller/cfg_node.h
@@ -0,0 +1,146 @@
+#ifndef DEVTOOLS_CROSSTOOL_AUTOFDO_LLVM_PROPELLER_CFG_NODE_H_
+#define DEVTOOLS_CROSSTOOL_AUTOFDO_LLVM_PROPELLER_CFG_NODE_H_
+
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "absl/functional/function_ref.h"
+#include "absl/strings/str_cat.h"
+#include "llvm/Object/ELFTypes.h"
+#include "propeller/cfg_edge.h"
+#include "propeller/cfg_edge_kind.h"
+#include "propeller/cfg_id.h"
+
+namespace propeller {
+
+class CFGEdge;
+class ControlFlowGraph;
+
+// All instances of CFGNode are owned by their cfg_.
+class CFGNode final {
+ public:
+  CFGNode(uint64_t addr, int bb_index, int bb_id, int size,
+          const llvm::object::BBAddrMap::BBEntry::Metadata &metadata,
+          int function_index, int freq = 0, int clone_number = 0,
+          int node_index = -1)
+      : inter_cfg_id_({function_index, {bb_index, clone_number}}),
+        bb_id_(bb_id),
+        node_index_(clone_number == 0 ? bb_index : node_index),
+        addr_(addr),
+        size_(size),
+        metadata_(metadata),
+        freq_(freq) {}
+
+  // Returns a clone of `*this` with the given assigned `clone_number`, but with
+  // zero frequency and empty edges.
+  std::unique_ptr<CFGNode> Clone(int clone_number, int node_index) const {
+    return std::make_unique<CFGNode>(addr_, bb_index(), bb_id_, size_,
+                                     metadata_, function_index(), /*freq=*/0,
+                                     clone_number, node_index);
+  }
+
+  // Returns a program-wide unique id for this node.
+  const propeller::InterCfgId &inter_cfg_id() const { return inter_cfg_id_; }
+  // Returns a cfg-wide unique id for this node.
+  const propeller::IntraCfgId &intra_cfg_id() const {
+    return inter_cfg_id_.intra_cfg_id;
+  }
+  propeller::FullIntraCfgId full_intra_cfg_id() const {
+    return {.bb_id = bb_id_, .intra_cfg_id = intra_cfg_id()};
+  }
+  uint64_t addr() const { return addr_; }
+  int bb_id() const { return bb_id_; }
+  int bb_index() const { return intra_cfg_id().bb_index; }
+  int node_index() const { return node_index_; }
+  int clone_number() const { return intra_cfg_id().clone_number; }
+  bool is_cloned() const { return clone_number() != 0; }
+  // Computes and returns the execution frequency of the node based on its
+  // edges.
+  int CalculateFrequency() const;
+  int size() const { return size_; }
+  bool is_landing_pad() const { return metadata_.IsEHPad; }
+  bool can_fallthrough() const { return metadata_.CanFallThrough; }
+  bool has_return() const { return metadata_.HasReturn; }
+  bool has_tail_call() const { return metadata_.HasTailCall; }
+  bool has_indirect_branch() const { return metadata_.HasIndirectBranch; }
+  int function_index() const { return inter_cfg_id_.function_index; }
+
+  const std::vector<CFGEdge *> &intra_outs() const { return intra_outs_; }
+  const std::vector<CFGEdge *> &intra_ins() const { return intra_ins_; }
+  const std::vector<CFGEdge *> &inter_outs() const { return inter_outs_; }
+  const std::vector<CFGEdge *> &inter_ins() const { return inter_ins_; }
+
+  void ForEachInEdgeRef(absl::FunctionRef<void(CFGEdge &edge)> func) const {
+    for (CFGEdge *edge : intra_ins_) func(*edge);
+    for (CFGEdge *edge : inter_ins_) func(*edge);
+  }
+
+  void ForEachOutEdgeRef(absl::FunctionRef<void(CFGEdge &edge)> func) const {
+    for (CFGEdge *edge : intra_outs_) func(*edge);
+    for (CFGEdge *edge : inter_outs_) func(*edge);
+  }
+
+  // Returns if this is the entry of the function.
+  bool is_entry() const { return bb_index() == 0; }
+
+  std::string GetName() const;
+
+  // Returns the edge from `*this` to `node` of kind `kind`, or `nullptr` if
+  // no such edge exists.
+  CFGEdge *GetEdgeTo(const CFGNode &node, propeller::CFGEdgeKind kind) const;
+
+  // Returns if there are any edge from `*this` to `node` of kind `kind`.
+  bool HasEdgeTo(const CFGNode &node, propeller::CFGEdgeKind kind) const {
+    return GetEdgeTo(node, kind) != nullptr;
+  }
+
+  template <typename Sink>
+  friend void AbslStringify(Sink &sink, const CFGNode &node);
+
+ private:
+  friend class ControlFlowGraph;
+
+  // Returns the bb index as a string to be used in the dot format.
+  std::string GetDotFormatLabel() const {
+    std::string result = absl::StrCat(bb_id_);
+    if (clone_number()) absl::StrAppend(&result, ".", clone_number());
+    return result;
+  }
+
+  void set_freq(int freq) { freq_ = freq; }
+
+  propeller::InterCfgId inter_cfg_id_;
+  // Fixed ID of the basic block, as defined by the compiler. Must be unique
+  // within each cfg. Will be used in the propeller profile.
+  const int bb_id_;
+  // Index of the node in its CFG's `nodes()`.
+  const int node_index_;
+  const int addr_;
+  int size_ = 0;
+  const llvm::object::BBAddrMap::BBEntry::Metadata metadata_;
+  int freq_ = 0;
+
+  std::vector<CFGEdge *> intra_outs_ = {};  // Intra function edges.
+  std::vector<CFGEdge *> intra_ins_ = {};   // Intra function edges.
+  std::vector<CFGEdge *> inter_outs_ = {};  // Calls to other functions.
+  std::vector<CFGEdge *> inter_ins_ = {};   // Returns from other functions.
+};
+
+template <typename Sink>
+inline void AbslStringify(Sink &sink, const propeller::CFGNode &node) {
+  absl::Format(&sink, "[id: %v, addr:%llu size: %d]", node.inter_cfg_id_,
+               node.addr_, node.size_);
+}
+
+template <typename Sink>
+inline void AbslStringify(Sink &sink, const CFGEdge &edge) {
+  absl::Format(&sink, "[%s -> %s, weight(%lld), type(%s), inter-section(%d)]",
+               edge.src()->GetName(), edge.sink()->GetName(), edge.weight(),
+               propeller::GetCfgEdgeKindString(edge.kind()),
+               edge.inter_section());
+}
+
+}  // namespace propeller
+#endif  // DEVTOOLS_CROSSTOOL_AUTOFDO_LLVM_PROPELLER_CFG_NODE_H_
