89b76021a03f9e6ae83e83bf4568f7d9f6340bf7
Daniel Hoekwater <hoekwater@google.com>
2024-11-12 13:27:26 -0800

    Add a sample source file for test data
    PiperOrigin-RevId: 695850675

---
+++ propeller/testdata/sample.c
@@ -0,0 +1,39 @@
+/* sample.c */
+volatile int count;
+
+static int goose() { return 13; }  // NOLINT
+
+__attribute__((noinline)) double this_is_very_code(double tt) {
+  volatile double dead = 3434343434,
+                  beaf = 56565656; /* Avoid compiler optimizing away */
+  return dead / beaf + beaf / dead + tt / 183;
+}
+
+__attribute__((noinline)) int compute_flag(int i) {
+  if (i % 10 < 4) /* ... in 40% of the iterations */
+    return i + 1;
+  return 0;
+}
+
+int sample1_func() { return 13; }
+
+int main(void) {
+  int i;
+  int flag;
+  volatile double x = 1212121212,
+                  y = 121212; /* Avoid compiler optimizing away */
+
+  for (i = 0; i < 800000000; i++) {
+    flag = compute_flag(i);
+
+    /* Some other code */
+    count++;
+
+    if (flag)
+      x += x / y + y / x; /* Execute expensive division if flag is set */
+    if (count % 137949234 == 183) {
+      x += this_is_very_code(count) + sample1_func();
+    }
+  }
+  return 0;
+}
