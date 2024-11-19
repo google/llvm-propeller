3966c39f38f66f068389667acc8f8e33fd9c1381
Propeller Team <propeller-dev@google.com>
2024-11-19 13:03:46 -0800

    Remove duplicate include in branch_aggregation.h
    PiperOrigin-RevId: 698117652

--- propeller/branch_aggregation.h
+++ propeller/branch_aggregation.h
@@ -3,7 +3,6 @@
 
 #include <cstdint>
 
-#include "propeller/binary_address_branch.h"
 #include "absl/algorithm/container.h"
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
