10c3295c57226e51bb8736a94b6f7e45838582ae
Daniel Hoekwater <hoekwater@google.com>
2025-04-02 11:01:49 -0700

    Update the absl version to latest
    PiperOrigin-RevId: 743204416

--- CMake/Absl/Absl.cmake
+++ CMake/Absl/Absl.cmake
@@ -15,7 +15,7 @@
 #
 
 # LINT.IfChange(version)
-set(_ABSL_VERSION 20240722.0)
+set(_ABSL_VERSION 20250127.1)
 # LINT.ThenChange(../../MODULE.bazel:abseil_version)
 
 set(propeller_absl_build_dir ${CMAKE_BINARY_DIR}/absl-build)
