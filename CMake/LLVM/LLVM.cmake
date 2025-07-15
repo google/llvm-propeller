a5ca99073e06af4659f2d5f5cc2be863b14f38e3
Daniel Hoekwater <hoekwater@google.com>
2025-07-15 14:50:24 -0700

    Bring LLVM up to date with the internal version
    PiperOrigin-RevId: 783481331

--- CMake/LLVM/LLVM.cmake
+++ CMake/LLVM/LLVM.cmake
@@ -16,7 +16,7 @@
 # Integrates LLVM into the build.
 
 # LINT.IfChange(commit_hash)
-set(_LLVM_HASH f58ce1152703ca753794b8cef36da30bd2668d0f)
+set(_LLVM_HASH 696c0f92e0fef955e331895cd8d65fb6cc080ad3)
 # LINT.ThenChange(../../WORKSPACE.bzlmod:llvm_commit_hash)
 
 set(propeller_llvm_build_dir ${CMAKE_BINARY_DIR}/llvm-build)
