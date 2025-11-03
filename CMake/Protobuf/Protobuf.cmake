8ac0c48ab8c2839695fcdf3020cc0cd9f8417c3a
Rahman Lavaee <rahmanl@google.com>
2025-11-03 11:09:16 -0800

    Bump protobuf version to 33.0.
    PiperOrigin-RevId: 827573575

--- CMake/Protobuf/Protobuf.cmake
+++ CMake/Protobuf/Protobuf.cmake
@@ -19,7 +19,7 @@
 cmake_minimum_required(VERSION 3.11)
 
 # LINT.IfChange(version)
-set(propeller_protobuf_version 30.2)
+set(propeller_protobuf_version 33.0)
 # LINT.ThenChange(../../MODULE.bazel:protobuf_version)
 
 # Declare and configure the external protobuf package.
